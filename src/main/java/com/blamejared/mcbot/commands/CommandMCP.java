9baf01cd41a7d96d24caacddd25182e3d61a20ec
tterrag1098 <tterrag1098@gmail.com>
2017-03-06 01:02:35 -0500

    Clean up CommandMCP

--- src/main/java/com/blamejared/mcbot/commands/CommandMCP.java
+++ src/main/java/com/blamejared/mcbot/commands/CommandMCP.java
@@ -1,10 +1,11 @@
 package com.blamejared.mcbot.commands;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -31,6 +32,7 @@ public class CommandMCP extends CommandBase {
     }
     
     private final LookupType type;
+    private final Random rand = new Random();
     
     public CommandMCP() {
         this(null);
@@ -56,99 +58,50 @@ public class CommandMCP extends CommandBase {
     
     @Override
     public void process(IMessage message, List<String> flags, List<String> args) throws CommandException {
-        switch (this.type) {
-            case FIELD:
-                try {
-                    File f = new File("fields.csv");
-                    if(!f.exists()) {
-                        f.createNewFile();
+        if (type == LookupType.CLASS) {
+        }
+
+        File f = this.type == LookupType.FIELD ? new File("fields.csv") : new File("methods.csv"); 
+        try {
+            if(!f.exists()) {
+                f.createNewFile();
+            }
+            String fieldName = message.getContent().split(" ")[1];
+            Scanner scan = new Scanner(f);
+            scan.useDelimiter("\n");
+            StringBuilder builder = new StringBuilder();
+            final EmbedBuilder embed = new EmbedBuilder();
+            final boolean[] found = {false};
+            scan.forEachRemaining(line -> {
+                System.out.println(line);
+                String[] info = line.split(",", -1);
+                if(info[0].equalsIgnoreCase(fieldName) || line.split(",")[1].equalsIgnoreCase(fieldName)) {
+                    if (found[0]) {
+                        builder.append("\n");
                     }
-                    String fieldName = message.getContent().split(" ")[1];
-                    Scanner scan = new Scanner(f).useDelimiter("\n");
-                    StringBuilder builder = new StringBuilder();
-                    final EmbedBuilder embed = new EmbedBuilder();
-                    final boolean[] found = {false};
-                    scan.forEachRemaining(line -> {
-                        System.out.println(line);
-                        String[] info = line.split(",", -1);
-                        if(info[0].equalsIgnoreCase(fieldName)) {
-                            found[0] = true;
-                            builder.append("SRG Name: " + info[0] + "\n");
-                            builder.append("Forge Name: " + info[1] + "\n");
-                            if(!info[3].trim().isEmpty()) {
-                                builder.append("Desc: " + info[3] + "\n");
-                            }
-                            builder.append("Side: " + (info[2].trim().equals("0") ? " Client" : info[2].trim().equals("1") ? " Server" : " Both") + "\n");
-                        } else if(line.split(",")[1].equalsIgnoreCase(fieldName)) {
-                            found[0] = true;
-                            builder.append("SRG Name: " + info[0] + "\n");
-                            builder.append("Forge Name: " + info[1] + "\n");
-                            if(!info[3].trim().isEmpty()) {
-                                builder.append("Desc: " + info[3] + "\n");
-                            }
-                            builder.append("Side: " + (info[2].trim().equals("0") ? " Client" : info[2].trim().equals("1") ? " Server" : " Both") + "\n");
-                        }
-                    });
-                    if(!found[0]) {
-                        builder.append("No information found!");
+                    found[0] = true;
+                    builder.append("SRG Name: " + info[0] + "\n");
+                    builder.append("Forge Name: " + info[1] + "\n");
+                    if(!info[3].trim().isEmpty()) {
+                        builder.append("Desc: " + info[3] + "\n");
                     }
-                    System.out.println(builder.toString());
-                    embed.ignoreNullEmptyFields();
-                    embed.withDesc(builder.toString());
-                    embed.withColor((int) (Math.random() * 0x1000000));
-                    embed.withTitle("Information on field: " + fieldName);
-                    message.getChannel().sendMessage(embed.build());
-                    scan.close();
-                } catch(IOException e) {
-                    e.printStackTrace();
+                    builder.append("Side: " + (info[2].trim().equals("0") ? " Client" : info[2].trim().equals("1") ? " Server" : " Both") + "\n");
                 }
-                break;
-            case METHOD:
-                try {
-                    File f = new File("methods.csv");
-                    if(!f.exists()) {
-                        f.createNewFile();
-                    }
-                    String fieldName = message.getContent().split(" ")[1];
-                    Scanner scan = new Scanner(f).useDelimiter("\n");
-                    StringBuilder builder = new StringBuilder();
-                    final EmbedBuilder embed = new EmbedBuilder();
-                    final boolean[] found = {false};
-                    scan.forEachRemaining(line -> {
-                        if(line.split(",")[0].equalsIgnoreCase(fieldName)) {
-                            found[0] = true;
-                            String[] info = line.split(",");
-                            builder.append("SRG Name: " + info[0] + "\n");
-                            builder.append("Forge Name: " + info[1] + "\n");
-                            if(!info[3].trim().isEmpty()) {
-                                builder.append("Desc: " + info[3] + "\n");
-                            }
-                            builder.append("Side: " + (info[2].trim().equals("0") ? " Client" : info[2].trim().equals("1") ? " Server" : " Both") + "\n");
-                        } else  if(line.split(",")[1].equalsIgnoreCase(fieldName)) {
-                            found[0] = true;
-                            String[] info = line.split(",");
-                            builder.append("SRG Name: " + info[0] + "\n");
-                            builder.append("Forge Name: " + info[1] + "\n");
-                            if(!info[3].trim().isEmpty()) {
-                                builder.append("Desc: " + info[3] + "\n");
-                            }
-                            builder.append("Side: " + (info[2].trim().equals("0") ? " Client" : info[2].trim().equals("1") ? " Server" : " Both") + "\n");
-                        }
-                    });
-                    if(!found[0]) {
-                        builder.append("No information found!");
-                    }
-                    System.out.println(builder.toString());
-                    embed.ignoreNullEmptyFields();
-                    embed.withDesc(builder.toString());
-                    embed.withColor((int) (Math.random() * 0x1000000));
-                    embed.withTitle("Information on method: " + fieldName);
-                    message.getChannel().sendMessage(embed.build());
-                    scan.close();
-                } catch(IOException e) {
-                    e.printStackTrace();
-                }
-                break;
+            });
+            scan.close();
+            if(!found[0]) {
+                builder.append("No information found!");
+            }
+            System.out.println(builder.toString());
+            embed.ignoreNullEmptyFields();
+            embed.withDesc(builder.toString());
+            rand.setSeed(builder.toString().hashCode());
+            embed.withColor(Color.HSBtoRGB(rand.nextFloat(), 1, 1));
+            embed.withTitle("Information on " + type + ": " + fieldName);
+            message.getChannel().sendMessage(embed.build());
+            scan.close();
+        } catch(IOException e) {
+            throw new CommandException(e);
         }
     }
     

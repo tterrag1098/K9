536da45ab6badc0c254e0c687b48a031c00d3a21
Jared <jaredlll08@gmail.com>
2017-03-07 20:11:50 +0200

    Added option to the slap command

--- src/main/java/com/blamejared/mcbot/commands/CommandSlap.java
+++ src/main/java/com/blamejared/mcbot/commands/CommandSlap.java
@@ -1,16 +1,25 @@
 package com.blamejared.mcbot.commands;
 
 import com.blamejared.mcbot.commands.api.*;
+import com.google.common.collect.Lists;
 import sx.blah.discord.handle.obj.IMessage;
 
-import java.util.List;
+import java.util.*;
 
 @Command
 public class CommandSlap extends CommandBase {
-    
+    private List<String> options = Lists.newArrayList();
+    private Random rand = new Random();
     public CommandSlap() {
         super("slap", false);
+        options.add(" with a large trout!");
+        options.add(" with a big bat!");
+        options.add(" with a frying pan!");
+        options.add(" like a little bitch!");
+    
+    
     }
+    
     
     @Override
     public void process(IMessage message, List<String> flags, List<String> args) throws CommandException {
@@ -19,7 +28,7 @@ public class CommandSlap extends CommandBase {
         }
         
         StringBuilder builder = new StringBuilder(message.getAuthor().getName());
-        builder.append(" slapped ").append(args.get(0)).append(" with a large trout!");
+        builder.append(" slapped ").append(args.get(0)).append(options.get(rand.nextInt(options.size())));
         if(!validateMessage(builder.toString())){
             message.getChannel().sendMessage("Unable to send a message mentioning a user!");
             return;

745737ccb19d4969e73db033f812aac3cca717e8
Jared <jaredlll08@gmail.com>
2017-04-04 00:57:43 +0200

    Added guild specific channels

--- src/main/java/com/blamejared/mcbot/MCBot.java
+++ src/main/java/com/blamejared/mcbot/MCBot.java
@@ -10,7 +10,7 @@ import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageDeleteEvent;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageUpdateEvent;
-import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.*;
 
 public class MCBot {
     
@@ -37,26 +37,36 @@ public class MCBot {
         return channel[0];
     }
     
+    public static IChannel getChannel(IGuild guild, String name) {
+        final IChannel[] channel = new IChannel[1];
+        guild.getChannels().forEach(chan -> {
+            if(chan.getName().equalsIgnoreCase(name)) {
+                channel[0] = chan;
+            }
+        });
+        return channel[0];
+    }
+    
     @EventSubscriber
     public void onMessageDeleted(MessageDeleteEvent event) {
-        if(event.getGuild().getName().equals("Modders Corner")) {
-            if(event.getChannel().getName().equalsIgnoreCase("bot-log")) {
-                return;
-            }
-            getChannel("bot-log").sendMessage(event.getAuthor().getName() + " Deleted message : ```" + event.getMessage().getContent().replaceAll("```", "") + "``` from channel: " + event.getChannel().getName());
+        IChannel botLog = getChannel(event.getGuild(), "bot-log");
+        if(event.getChannel().getName().equalsIgnoreCase("bot-log")) {
+            return;
+        }
+        if(botLog!=null){
+            getChannel(event.getGuild(),"bot-log").sendMessage(event.getAuthor().getName() + " Deleted message : ```" + event.getMessage().getContent().replaceAll("```", "") + "``` from channel: " + event.getChannel().getName());
         }
     }
     
     @EventSubscriber
     public void onMessageEdited(MessageUpdateEvent event) {
-        if(event.getGuild().getName().equals("Modders Corner")) {
-            if(event.getChannel().getName().equalsIgnoreCase("bot-log")) {
-                return;
-            }
-            if(event.getAuthor().isBot()) {
-                return;
-            }
-            getChannel("bot-log").sendMessage(event.getAuthor().getName() + " Edited message: ```" + event.getOldMessage().getContent().replaceAll("```", "") + "``` -> ```" + event.getNewMessage().getContent().replaceAll("```", "") + "``` from channel: " + event.getChannel().getName());
+    
+        IChannel botLog = getChannel(event.getGuild(), "bot-log");
+        if(event.getChannel().getName().equalsIgnoreCase("bot-log")) {
+            return;
+        }
+        if(botLog!=null){
+            getChannel(event.getGuild(),"bot-log").sendMessage(event.getAuthor().getName() + " Edited message: ```" + event.getOldMessage().getContent().replaceAll("```", "") + "``` -> ```" + event.getNewMessage().getContent().replaceAll("```", "") + "``` from channel: " + event.getChannel().getName());
         }
     }
     

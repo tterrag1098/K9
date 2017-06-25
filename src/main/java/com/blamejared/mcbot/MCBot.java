b85a99b8c10e4937e2497585904b3f0170f65fd7
Jared <jaredlll08@gmail.com>
2017-06-25 02:42:48 +0200

    Added pastebin to gist conversion

--- src/main/java/com/blamejared/mcbot/MCBot.java
+++ src/main/java/com/blamejared/mcbot/MCBot.java
@@ -4,10 +4,17 @@ import com.blamejared.mcbot.commands.api.CommandRegistrar;
 import com.blamejared.mcbot.irc.MCBotIRC;
 import com.blamejared.mcbot.listeners.ChannelListener;
 import com.blamejared.mcbot.mcp.DataDownloader;
+import org.eclipse.egit.github.core.*;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.GistService;
 import sx.blah.discord.api.*;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.*;
 import sx.blah.discord.handle.obj.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class MCBot {
     
@@ -73,7 +80,7 @@ public class MCBot {
     }
     
     @EventSubscriber
-    public void onMessageRecieved(MessageReceivedEvent event) {
+    public void onMessageRecieved(MessageReceivedEvent event) throws IOException {
         if(event.getGuild().getName().equals("Modders Corner")) {
             if(event.getMessage().getChannel().getName().equals("general-discussion")) {
                 boolean isGif = false;
@@ -86,12 +93,33 @@ public class MCBot {
                 if(event.getMessage().getContent().contains("https://tenor.co")) {
                     isGif = true;
                 }
-        
+                
                 if(isGif) {
                     event.getMessage().delete();
                     event.getChannel().sendMessage("Sorry! GIFs are not allowed in this chat! Head to <#235949539138338816>");
                 }
             }
+        }
+        if(event.getMessage().getFormattedContent().contains("https://pastebin.com/")) {
+            boolean valid = false;
+            StringBuilder urls = new StringBuilder("Pastebin -> Gist: \n");
+            for(String s : event.getMessage().getFormattedContent().split(" ")) {
+                if(s.startsWith("https://pastebin.com/") && !s.endsWith("/")) {
+                    valid = true;
+                    URL url = new URL("https://pastebin.com/raw/" + event.getMessage().getFormattedContent().split("https://pastebin.com/")[1].split(" ")[0]);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                    StringBuilder content = new StringBuilder();
+                    in.lines().forEachOrdered(line -> content.append(line).append("\n"));
+                    GitHubClient client = new GitHubClient();
+                    Gist gist = new Gist().setDescription("Pastebin Conversion");
+                    GistFile file = new GistFile().setFilename("Converted.java").setContent(content.toString());
+                    gist.setFiles(Collections.singletonMap("Pastebin conversion", file));
+                    gist = new GistService(client).createGist(gist);
+                    urls.append(gist.getHtmlUrl()).append("\n");
+                }
+            }
+            if(valid)
+                event.getChannel().sendMessage(urls.toString());
         }
     }
 }

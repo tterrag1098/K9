d633dc9da386c1dab866b773a7d0478ad07bc3a6
tterrag1098 <tterrag1098@gmail.com>
2017-06-27 04:23:46 -0400

    Fix serialization of quotes

--- src/main/java/com/blamejared/mcbot/commands/CommandQuote.java
+++ src/main/java/com/blamejared/mcbot/commands/CommandQuote.java
@@ -1,7 +1,8 @@
 package com.blamejared.mcbot.commands;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import com.blamejared.mcbot.commands.api.Command;
@@ -9,15 +10,13 @@ import com.blamejared.mcbot.commands.api.CommandException;
 import com.blamejared.mcbot.commands.api.CommandPersisted;
 import com.google.gson.reflect.TypeToken;
 
-import gnu.trove.map.TIntObjectMap;
-import gnu.trove.map.hash.TIntObjectHashMap;
 import sx.blah.discord.handle.obj.IMessage;
 
 @Command
-public class CommandQuote extends CommandPersisted<TIntObjectMap<String>> {
+public class CommandQuote extends CommandPersisted<Map<Integer, String>> {
     
     public CommandQuote() {
-        super("quote", false, TIntObjectHashMap::new);
+        super("quote", false, HashMap::new);
 //        quotes.put(id++, "But noone cares - HellFirePVP");
 //        quotes.put(id++, "CRAFTTWEAKER I MEANT CRAFTTWEAKER - Drullkus");
 //        quotes.put(id++, "oh yeah im dumb - Kit");
@@ -26,8 +25,8 @@ public class CommandQuote extends CommandPersisted<TIntObjectMap<String>> {
     }
     
     @Override
-    protected TypeToken<TIntObjectMap<String>> getDataType() {
-        return new TypeToken<TIntObjectMap<String>>(){};
+    protected TypeToken<Map<Integer, String>> getDataType() {
+        return new TypeToken<Map<Integer, String>>(){};
     }
 
     Random rand = new Random();
@@ -38,15 +37,14 @@ public class CommandQuote extends CommandPersisted<TIntObjectMap<String>> {
             if(flag.equalsIgnoreCase("ls")) {
                 StringBuilder builder = new StringBuilder();
                 builder.append("List of quotes:\n");
-                storage.get(message).forEachEntry((k, v) -> {
+                storage.get(message).forEach((k, v) -> {
                     builder.append(k).append(") ").append(v).append("\n");
-                    return true;
                 });
                 message.getChannel().sendMessage(builder.toString());
                 return;
             } else if(flag.equalsIgnoreCase("add")) {
-                TIntObjectMap<String> quotes = storage.get(message);
-                quotes.put(Arrays.stream(quotes.keys()).max().orElse(1), escapeMentions(message.getGuild(), message.getContent().substring("!quote -add ".length())));
+                Map<Integer, String> quotes = storage.get(message);
+                quotes.put(quotes.keySet().stream().max(Integer::compare).orElse(1), escapeMentions(message.getGuild(), message.getContent().substring("!quote -add ".length())));
                 message.getChannel().sendMessage("Added quote!");
                 return;
             } else if(flag.startsWith("remove=")) {
@@ -57,7 +55,7 @@ public class CommandQuote extends CommandPersisted<TIntObjectMap<String>> {
             }
         }
         if(args.isEmpty()) {
-            int[] keys = storage.get(message).keys();
+            Integer[] keys = storage.get(message).keySet().toArray(new Integer[0]);
             if (keys.length == 0) {
                 throw new CommandException("There are no quotes!");
             }

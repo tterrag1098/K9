a97317ae6fe022f32b195072d50924faa0d9e7bf
tterrag1098 <tterrag1098@gmail.com>
2017-09-06 18:49:37 -0400

    Fix URL encoding of lmgtfy output

--- src/main/java/com/blamejared/mcbot/commands/CommandLMGTFY.java
+++ src/main/java/com/blamejared/mcbot/commands/CommandLMGTFY.java
@@ -1,10 +1,14 @@
 package com.blamejared.mcbot.commands;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import com.blamejared.mcbot.commands.api.Command;
 import com.blamejared.mcbot.commands.api.CommandBase;
 import com.blamejared.mcbot.commands.api.CommandContext;
 import com.blamejared.mcbot.commands.api.CommandException;
 import com.blamejared.mcbot.commands.api.Flag;
+import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 
@@ -23,9 +27,13 @@ public class CommandLMGTFY extends CommandBase {
             throw new CommandException("Not enough arguments.");
         }
         int iie = ctx.getFlags().containsKey(FLAG_IE) ? 1 : 0;
-        StringBuilder arg = new StringBuilder("http://lmgtfy.com/?iie=").append(iie).append("&q=");
-        arg.append(Joiner.on('+').join(ctx.getArgs()));
-        ctx.reply(arg.toString());
+        StringBuilder url = new StringBuilder("<http://lmgtfy.com/?iie=").append(iie).append("&q=");
+        String arg = Joiner.on(' ').join(ctx.getArgs());
+        try {
+            ctx.reply(url.append(URLEncoder.encode(ctx.sanitize(arg), Charsets.UTF_8.name())).append(">").toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new CommandException(e);
+        }
     }
     
     public String getUsage() {

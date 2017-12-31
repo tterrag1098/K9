9d73a3e9fc4ffb3f051ddba10344003b214ae865
tterrag <tterrag1098@gmail.com>
2017-12-31 00:33:42 -0500

    Add clojail/clojure libs, and clojure command

---
+++ src/main/java/com/blamejared/mcbot/commands/CommandClojure.java
@@ -0,0 +1,56 @@
+package com.blamejared.mcbot.commands;
+
+import java.io.StringWriter;
+import java.util.concurrent.TimeoutException;
+
+import com.blamejared.mcbot.commands.api.Command;
+import com.blamejared.mcbot.commands.api.CommandBase;
+import com.blamejared.mcbot.commands.api.CommandContext;
+import com.blamejared.mcbot.commands.api.CommandException;
+
+import clojure.java.api.Clojure;
+import clojure.lang.IFn;
+import clojure.lang.PersistentArrayMap;
+import clojure.lang.Var;
+
+@Command
+public class CommandClojure extends CommandBase {
+
+    private static final SentenceArgument ARG_EXPR = new SentenceArgument("expression", "The clojure expression to evaluate.", true);
+    
+    private final IFn sandbox;
+    
+    public CommandClojure() {
+        super("clj", false);
+        
+        IFn require = Clojure.var("clojure.core", "require");
+        require.invoke(Clojure.read("clojail.core"));
+        require.invoke(Clojure.read("clojail.jvm"));
+        require.invoke(Clojure.read("clojail.testers"));
+
+        IFn sandboxfn = Clojure.var("clojail.core", "sandbox");
+        Var tester = (Var) Clojure.var("clojail.testers", "secure-tester");
+        this.sandbox = (IFn) sandboxfn.invoke(tester.getRawRoot(), Clojure.read(":timeout"), 2000L);
+    }
+    
+    @Override
+    public void process(CommandContext ctx) throws CommandException {
+        try {
+            StringWriter sw = new StringWriter();
+            sandbox.invoke(Clojure.read("(prn " + ctx.getArg(ARG_EXPR) + ")"), new PersistentArrayMap(new Object[] {Clojure.var("clojure.core", "*out*"), sw}));
+            String output = sw.getBuffer().toString();
+            ctx.reply(output);
+        } catch (Exception e) {
+            // Can't catch TimeoutException because invoke() does not declare it as a possible checked exception
+            if (e instanceof TimeoutException) {
+                throw new CommandException("That took too long to execute!");
+            }
+            throw new CommandException(e);
+        }
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Evaluate some clojure code in a sandboxed REPL.";
+    }
+}

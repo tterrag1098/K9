bcb52ac3729184d42b6d88210a64b422b80d2d9b
tterrag <tterrag1098@gmail.com>
2018-08-21 15:11:37 -0400

    Fix NPE when looking up unnammed members without a hierarchy

--- src/main/java/com/tterrag/k9/mcp/MappingDatabase.java
+++ src/main/java/com/tterrag/k9/mcp/MappingDatabase.java
@@ -267,7 +267,7 @@ public class MappingDatabase {
         // Remove srg matches that are mapped
         for (Entry<String, ISrgMapping> e : srgMatches.entrySet()) {
             if (!srgToInfo.containsKey(e.getKey())) {
-                if (parent == null | Strings.nullToEmpty(e.getValue().getOwner()).endsWith(parent)) {
+                if (parent == null || Strings.nullToEmpty(e.getValue().getOwner()).endsWith(parent)) {
                     srgToInfo.put(e.getKey(), new MemberInfo(e.getValue(), null));
                 }
             }

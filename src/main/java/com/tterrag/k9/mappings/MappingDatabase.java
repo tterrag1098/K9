ff9acc46602e240317d4e3586b9e54e0dd6ac2d8
tterrag <tterrag1098@gmail.com>
2019-02-18 14:21:46 -0500

    Dedupe lookup results when searching across multiple name types

--- src/main/java/com/tterrag/k9/mappings/MappingDatabase.java
+++ src/main/java/com/tterrag/k9/mappings/MappingDatabase.java
@@ -12,11 +12,11 @@ public interface MappingDatabase<@NonNull T extends Mapping> {
     void reload() throws IOException, NoSuchVersionException;
     
     default Collection<T> lookup(String search) {
-        return Arrays.stream(MappingType.values()).flatMap(type -> lookup(type, search).stream()).collect(Collectors.toList());
+        return Arrays.stream(MappingType.values()).flatMap(type -> lookup(type, search).stream()).distinct().collect(Collectors.toList());
     }
     
     default Collection<T> lookup(MappingType type, String search) {
-        return Arrays.stream(NameType.values()).flatMap(by -> lookup(by, type, search).stream()).collect(Collectors.toList());
+        return Arrays.stream(NameType.values()).flatMap(by -> lookup(by, type, search).stream()).distinct().collect(Collectors.toList());
     }
 
     Collection<T> lookup(NameType by, MappingType type);

85712ce5ad651c4ae478f289971228e363ce108e
tterrag <tterrag1098@gmail.com>
2019-06-19 00:43:55 -0400

    Improve PassthroughException to cover (hopefully) all cases

--- src/main/java/com/tterrag/k9/util/PassthroughException.java
+++ src/main/java/com/tterrag/k9/util/PassthroughException.java
@@ -1,11 +1,19 @@
 package com.tterrag.k9.util;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import lombok.experimental.Delegate;
 
 @SuppressWarnings("serial")
 public class PassthroughException extends Exception {
+    
+    private interface Excludes {
+        Throwable fillInStackTrace();
+        
+        void addSuppressed(Throwable exception);
+        
+        Throwable[] getSuppressed();
+    }
 
+    @Delegate(excludes = Excludes.class)
     private final Throwable parent;
 
     public PassthroughException(String message) {
@@ -15,31 +23,9 @@ public class PassthroughException extends Exception {
     public PassthroughException(Throwable parent) {
         this.parent = parent;
     }
-    
-    // Hopefully these are enough
-
-    @Override
-    public String getMessage() {
-        return parent.getMessage();
-    }
-
+ 
     @Override
     public String toString() {
         return parent.getClass() == Exception.class ? getMessage() : parent.toString();
-    }
-
-    @Override
-    public synchronized Throwable getCause() {
-        return parent.getCause();
-    }
-
-    @Override
-    public void printStackTrace(PrintWriter arg0) {
-        parent.printStackTrace(arg0);
-    }
-
-    @Override
-    public void printStackTrace(PrintStream arg0) {
-        parent.printStackTrace(arg0);
     }
 }

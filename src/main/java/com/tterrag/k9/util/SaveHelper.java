40f5262430de0b326c89fdefc39ae4d790ec84d0
tterrag <tterrag1098@gmail.com>
2019-09-09 00:10:14 -0400

    Add error handling to SaveHelper to back up data in case of read error

--- src/main/java/com/tterrag/k9/util/SaveHelper.java
+++ src/main/java/com/tterrag/k9/util/SaveHelper.java
@@ -7,14 +7,18 @@ import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
 
+import org.apache.commons.io.FileUtils;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.tterrag.k9.util.annotation.Nullable;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 public class SaveHelper<T> {
 	
 	private final File parentFolder;
@@ -31,10 +35,14 @@ public class SaveHelper<T> {
 		}
 		return exists;
 	}
+	
+	private File getFile(String file) {
+	    return new File(parentFolder, file);
+	}
 
 	@SneakyThrows
 	public Reader getReader(String file) {
-		File f = new File(parentFolder, file);
+		File f = getFile(file);
 		if (checkExists(f, false)) {
 		    return new FileReader(f);
 		} else {
@@ -53,21 +61,33 @@ public class SaveHelper<T> {
 	
 	@SneakyThrows
 	public FileWriter getWriter(String file) {
-		File f = new File(parentFolder, file);
+		File f = getFile(file);
 		checkExists(f, true);
 		return new FileWriter(f);
 	}
 	
 	public T fromJson(String file, Class<T> type) {
 		return fromJson(file, TypeToken.get(type));
-	}
+    }
 
-	public T fromJson(String file, TypeToken<T> type) {
-		T ret = gson.fromJson(getReader(file), type.getType());
-		if (ret == null) {
-		    ret = defaultValue;
-		}
-		return ret;
+    public T fromJson(String file, TypeToken<T> type) {
+        try {
+            T ret = gson.fromJson(getReader(file), type.getType());
+            if (ret == null) {
+                ret = defaultValue;
+            }
+            return ret;
+        } catch (Exception e) {
+            log.error("Failed to load data from file {}, copying to backup: {}", file, file + ".bak");
+            log.error("Error trace: ", e);
+            try {
+                FileUtils.copyFile(getFile(file), getFile(file + ".bak"));
+                return defaultValue;
+            } catch (IOException e1) {
+                log.error("Exception copying file, data loss could occur!", e1);
+                throw new RuntimeException(e1);
+            }
+        }
 	}
 	
 	public void writeJson(String file, T toWrite) {

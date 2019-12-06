adb569084afcc82e33f61aa98709e2938e3d6be3
tterrag <tterrag1098@gmail.com>
2019-12-06 14:25:24 -0500

    Add error handling to increment listener and handle long messages

--- src/main/java/com/tterrag/k9/listeners/IncrementListener.java
+++ src/main/java/com/tterrag/k9/listeners/IncrementListener.java
@@ -14,12 +14,13 @@ import com.tterrag.k9.util.Patterns;
 import com.tterrag.k9.util.SaveHelper;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public enum IncrementListener {
     
     INSTANCE;
-    
     
     private static final SaveHelper<Map<String, Long>> saveHelper = new SaveHelper<>(new File("counts"), new Gson(), new HashMap<>());
     private static final GuildStorage<Map<String, Long>> counts = new GuildStorage<>(
@@ -38,7 +39,11 @@ public enum IncrementListener {
                  long current = counts.get(event.getMessage()).block().merge(key, incr, (a, b) -> a + b);
                  saveHelper.writeJson(event.getGuildId().get().asLong() + ".json", counts.get(event.getGuildId().get()));
                  return event.getMessage().getChannel().transform(
-                         Monos.flatZipWhen(c -> CommandContext.sanitize(c, key + " == " + current), (chan, content) -> chan.createMessage(spec -> spec.setContent(content))));
-             }).thenReturn(event);
+                         Monos.flatZipWhen(c -> CommandContext.sanitize(c, key + " == " + current).filter(s -> s.length() <= 2000), (chan, content) -> chan.createMessage(spec -> spec.setContent(content))));
+             })
+             .doOnError(t -> log.error("Exception processing increment:", t))
+             .onErrorResume(e -> event.getMessage().getChannel().flatMap(c -> c.createMessage("Exception processing increment: " + e.toString())))
+             .onErrorResume($ -> Mono.empty()) // Ignore errors from posting errors
+             .thenReturn(event);
     }
 }

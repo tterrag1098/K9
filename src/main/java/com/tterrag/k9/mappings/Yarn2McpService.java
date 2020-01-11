17546347c9403db21a838a298c3a479c37aa8559
tterrag <tterrag1098@gmail.com>
2020-01-11 01:02:38 -0500

    Refactor stable yarn mappings to not publish new snapshots daily
    Publish all Yarn2MCP subscribers on elastic

--- src/main/java/com/tterrag/k9/mappings/Yarn2McpService.java
+++ src/main/java/com/tterrag/k9/mappings/Yarn2McpService.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -46,6 +47,7 @@ import com.tterrag.k9.util.Monos;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.util.function.Tuple2;
 
 @Slf4j
@@ -88,16 +90,18 @@ public class Yarn2McpService {
         // These either shouldn't change (if yarn is on a newer version) or will get updated
         // by the daily export (if yarn is on the same version).
         return McpDownloader.INSTANCE.getLatestMinecraftVersion(false)
-                .flatMap(version -> publishIfNotExists(version, YARN, this::publishMappings))
+                .publishOn(Schedulers.elastic())
+                .flatMap(version -> publishIfNotExists(version, true, YARN, this::publishMappings))
                 // Then run initial output of the latest yarn version if no file exists for today
                 .then(YarnDownloader.INSTANCE.getLatestMinecraftVersion(true))
-                .flatMap(version -> publishIfNotExists(version, YARN, this::publishMappings).thenReturn(version))
+                .flatMap(version -> publishIfNotExists(version, false, YARN, this::publishMappings).thenReturn(version))
                 // Also publish mixed mappings
-                .flatMap(version -> publishIfNotExists(version, MIXED, v -> publishMixedMappings(MIXED_VERSION, v)))
+                .flatMap(version -> publishIfNotExists(version, true, MIXED, (v, $) -> publishMixedMappings(MIXED_VERSION, v)))
                 // Then begin a periodic publish every day at midnight
                 .thenMany(Flux.interval(Duration.between(Instant.now(), Instant.now().plus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS)), Duration.ofDays(1)))
                 .flatMap(tick -> YarnDownloader.INSTANCE.getLatestMinecraftVersion(true)
-                        .flatMap(version -> publishMappings(version).thenReturn(version))
+                        .publishOn(Schedulers.elastic())
+                        .flatMap(version -> publishMappings(version, true).thenReturn(version))
                         .flatMap(version -> publishMixedMappings(MIXED_VERSION, version))
                         .thenReturn(tick)
                         .doOnError(t -> log.error("Error performing periodic mapping push", t))
@@ -105,10 +109,10 @@ public class Yarn2McpService {
                 .then();
     }
     
-    private Mono<Void> publishIfNotExists(String version, String name, Function<String, Mono<Void>> func) {
-        return Mono.fromSupplier(() -> getOutputFile(version, name))
+    private Mono<Void> publishIfNotExists(String version, boolean stable, String name, BiFunction<String, Boolean, Mono<Void>> func) {
+        return Mono.fromSupplier(() -> getOutputFile(version, stable, name))
             .filter(p -> !p.toFile().exists())
-            .flatMap($ -> func.apply(version));
+            .flatMap($ -> func.apply(version, stable));
     }
     
     private Mono<SrgDatabase> getSrgs(String version) {
@@ -130,11 +134,11 @@ public class Yarn2McpService {
                         .filter(e -> e.getKey().getName() != null || e.getValue().getName() != null)
                         .map(e -> toCsv(e.getKey(), e.getValue()))
                         .collectList()
-                        .flatMap(csv -> writeToFile(yarnVersion, gf.key(), csv, MIXED)))
+                        .flatMap(csv -> writeToFile(yarnVersion, false, gf.key(), csv, MIXED)))
                 .then();
     }
     
-    private Mono<Void> publishMappings(String version) {
+    private Mono<Void> publishMappings(String version, boolean stable) {
         return Mono.zip(getSrgs(version), YarnDownloader.INSTANCE.getDatabase(version))
                 .doOnNext($ -> log.info("Publishing yarn-over-mcp for MC " + version))
                 .as(Monos.groupWith(Flux.just(SUPPORTED_TYPES), (type, t) -> findMatching(type, t.getT1(), t.getT2())))
@@ -142,7 +146,7 @@ public class Yarn2McpService {
                         .filter(e -> e.getKey().getName() != null || e.getValue().getName() != null)
                         .map(e -> toCsv(e.getKey(), e.getValue()))
                         .collectList()
-                        .flatMap(csv -> writeToFile(version, gf.key(), csv, YARN)))
+                        .flatMap(csv -> writeToFile(version, stable, gf.key(), csv, YARN)))
                 .then();
     }
 
@@ -202,20 +206,20 @@ public class Yarn2McpService {
         return null;
     }
     
-    private Path getOutputFile(String version, String name) {
-        String channel = "mcp_snapshot";
+    private Path getOutputFile(String version, boolean stable, String name) {
+        String channel = stable ? "mcp_stable" : "mcp_snapshot";
         OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
-        String snapshot = String.format("%04d%02d%02d-%s-%s", now.getYear(), now.getMonthValue(), now.getDayOfMonth(), name, version);
+        String snapshot = stable ? name + "-" + version : String.format("%04d%02d%02d-%s-%s", now.getYear(), now.getMonthValue(), now.getDayOfMonth(), name, version);
         return output
                 .resolve(channel)
                 .resolve(snapshot)
                 .resolve(channel + "-" + snapshot + ".zip");
     }
     
-    private Mono<Void> writeToFile(String version, MappingType type, List<String> csv, String name) {
+    private Mono<Void> writeToFile(String version, boolean stable, MappingType type, List<String> csv, String name) {
         return Mono.fromRunnable(() -> {
             try {
-                Path zip = getOutputFile(version, name);
+                Path zip = getOutputFile(version, stable, name);
                 File parent = zip.getParent().toFile();
                 if (!parent.mkdirs() && !parent.exists()) {
                     throw new IllegalStateException("Could not create maven directory: " + zip.getParent());
